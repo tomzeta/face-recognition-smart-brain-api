e1356b50a8e800b8dfe567d18d73ebeed9d001ab
tomzeta <tommy.zambon@gmail.com>
2020-01-06 21:36:16 +0100

    changed get root call

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
 
+app.get('/',(req,res) => {res.send('it is working!')})
+
 app.post('/signin', (req,res) => {signin.handleSignIn(req,res,db,bcrypt)})
 
 app.post('/register',(req, res) => {register.handleRegister(req,res,db,bcrypt)})
